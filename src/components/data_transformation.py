833f888a63a51fba413cdef0cbe0dd8bd6d897fa
AnayMishra2006 <anaymishra20212105@gmail.com>
2025-08-30 00:40:22 +0530

    Data Transformation done

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -13,7 +13,7 @@ import os
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_ob_file_path=os.path.join('artifact',"preprocessor.pkl")
+    preprocessor_ob_file_path=os.path.join('artifacts',"preprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
@@ -32,16 +32,16 @@ class DataTransformation:
 
             num_pipeline=Pipeline(
                 steps=[
-                    ("imputer",SimpleImputer(strategy="median"))
-                    ("scaler",StandardScaler())
+                    ("imputer",SimpleImputer(strategy="median")),
+                    ("scaler",StandardScaler()),
                 ]
             )
 
             cat_pipeline=Pipeline(
                 steps=[
-                    ("imputer",SimpleImputer(strategy="most_frequent"))
-                    ("one_hot_encoder",OneHotEncoder())
-                    ("scaler",StandardScaler())
+                    ("imputer",SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder",OneHotEncoder()),
+                    ("scaler",StandardScaler(with_mean=False)),
                 ]
             )
 
@@ -50,7 +50,7 @@ class DataTransformation:
             
             preprocessor=ColumnTransformer(
                 [
-                    ("num_pipeline",num_pipeline,numerical_columns)
+                    ("num_pipeline",num_pipeline,numerical_columns),
                     ("cat_pipeline",cat_pipeline,categorical_columns)
                 ]
             )
@@ -69,7 +69,7 @@ class DataTransformation:
             logging.info("Obtaining preprocessing object")
             preprocessing_obj=self.get_data_transformer_object()
 
-            target_column_name="math score"
+            target_column_name="math_score"
             numerical_columns=["writing_score","reading_score"]
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
@@ -97,12 +97,12 @@ class DataTransformation:
 
             save_object(
                 file_path=self.data_transformation_config.preprocessor_ob_file_path,
-                obj=preprocessing_obj
+                obj=preprocessing_obj,
             )
             return(
                 train_arr,
                 test_arr,
                 self.data_transformation_config.preprocessor_ob_file_path,
             )
-        except:
-            pass
+        except Exception as e:
+            raise CustomException(e,sys)
